aabda5258d3d345776848a293b3140892f17b09b
ArenRedd <ArenRedd@users.noreply.github.com>
2025-08-05 13:59:24 +0000

    deploy: 26a6e26c250431f29ad80a74a219e1c47587d21f

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1754402170',resources: [
+    cacheName: 'chirpy-1754402363',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
