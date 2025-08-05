79354554e34285fde059e18da66889372b3587df
ArenRedd <anandrammohan9952@gmail.com>
2025-08-05 19:25:33 +0530

    Add Chirpy GitHub Actions deploy workflow

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1754401307',resources: [
+    cacheName: 'chirpy-1754402114',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
